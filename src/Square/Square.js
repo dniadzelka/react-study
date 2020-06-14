e9cfa1a69e55cf96b2222a5ad59f952fbea0b31b
Dmitry Nedelko <week62@gmail.com>
2020-06-14 12:20:12 +0300

    implemented tic-tac-toe game

---
+++ src/Square/Square.js
@@ -0,0 +1,12 @@
+import './Square.css';
+import React from 'react';
+
+
+export function Square(props) {
+    return (
+        <button className="square" onClick={props.onSquareClick}>
+            {props.value}
+        </button>
+    );
+
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+import ReactDOM from 'react-dom';
+import React from 'react';
+import './index.css';
+import { Game } from './Game/Game';
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
